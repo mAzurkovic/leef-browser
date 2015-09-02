e0265422f75d07c939482c6418049664eb5ea0c8
mAzurkovic <mattias@zurkovic.com>
2015-09-02 11:14:01 -0700

    window

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+#===================================#
+# Author: Mattias A. Zurkovic       #
+# ----------------------------------#
+# This is the main.py file for      #
+# the Leef browser.                 #
+#===================================#
+
 import sys
 from Tkinter import *
 
@@ -7,6 +14,11 @@ def __init__():
 # Function for creating/initializing the GUI for the browser
 def generate_win():
   print('Welcome to Leef. An open-source and light-weight browser for the modern web.')
+  
+  root = Tk()
+  root.title('Leef Browser')
 
+  root.mainloop()
 
+# Initialize main.py => run program
 __init__()
