71417dbbaf8bbbf325681c1a4a3762f88235b159
mAzurkovic <mattias@zurkovic.com>
2015-09-02 08:48:19 -0700

    init

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys
+
+def __init__():
+  generate_win()
+
+# Function for creating/initializing the GUI for the browser
+def generate_win():
+  print('Welcome to Leef. An open-source and light-weight browser for the modern web.')
+
+
+__init__()
